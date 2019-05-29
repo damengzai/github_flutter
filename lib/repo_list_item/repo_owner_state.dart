996b88229da626b8e2966bd9fba331b26198f6d6
shumengma <shumeng.ma@qunar.com>
2019-05-29 20:40:24 +0800

    构造函数

--- lib/repo_list_item/repo_owner_state.dart
+++ lib/repo_list_item/repo_owner_state.dart
@@ -19,26 +19,48 @@ class RepoOwnerState implements Cloneable<RepoOwnerState> {
   String received_events_url;
   String type; // "User",
   bool site_admin; // false
+  
+  RepoOwnerState({
+    this.login,
+    this.id,
+    this.node_id,
+    this.avatar_url,
+    this.gravatar_id,
+    this.url,
+    this.html_url,
+    this.followers_url,
+    this.following_url,
+    this.gists_url,
+    this.starred_url,
+    this.subscriptions_url,
+    this.organizations_url,
+    this.repos_url,
+    this.events_url,
+    this.received_events_url,
+    this.type,
+    this.site_admin,
+  });
+
   @override
   RepoOwnerState clone() {
     return RepoOwnerState()
-    ..login = login
-    ..id = id
-    ..node_id = node_id
-    ..avatar_url = avatar_url
-    ..gravatar_id = gravatar_id
-    ..url = url
-    ..html_url = html_url
-    ..followers_url = followers_url
-    ..following_url = following_url
-    ..gists_url = gists_url
-    ..starred_url = starred_url
-    ..subscriptions_url = subscriptions_url
-    ..organizations_url = organizations_url
-    ..repos_url = repos_url
-    ..events_url = events_url
-    ..received_events_url = received_events_url
-    ..type = type
-    ..site_admin = site_admin;
+      ..login = login
+      ..id = id
+      ..node_id = node_id
+      ..avatar_url = avatar_url
+      ..gravatar_id = gravatar_id
+      ..url = url
+      ..html_url = html_url
+      ..followers_url = followers_url
+      ..following_url = following_url
+      ..gists_url = gists_url
+      ..starred_url = starred_url
+      ..subscriptions_url = subscriptions_url
+      ..organizations_url = organizations_url
+      ..repos_url = repos_url
+      ..events_url = events_url
+      ..received_events_url = received_events_url
+      ..type = type
+      ..site_admin = site_admin;
   }
 }
