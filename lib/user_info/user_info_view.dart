f080d7ff279645b33721cf0bb35eca7b28a91be3
shumengma <shumeng.ma@qunar.com>
2019-08-04 18:16:27 +0800

    优化布局

--- lib/user_info/user_info_view.dart
+++ lib/user_info/user_info_view.dart
@@ -8,75 +8,63 @@ Widget buildView(
     UserInfoState state, Dispatch dispatch, ViewService viewService) {
   return SafeArea(
     child: Container(
-      margin: const EdgeInsets.all(4),
-      child: Center(
-        child: Row(
+        margin: const EdgeInsets.all(4),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          key: Key('user_info'),
           children: <Widget>[
-            Column(
-              key: Key('user_info'),
-              children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(6.0),
-                child: FadeInImage.assetNetwork(
-                  width: 120,
-                  height: 120,
-                  placeholder:
-                      "https://avatars0.githubusercontent.com/u/4232933?s=400&v=4",
-                  image: state.avatar_url ??
-                      "https://avatars0.githubusercontent.com/u/4232933?s=400&v=4",
-                ),
+            Padding(
+              padding: EdgeInsets.all(6.0),
+              child: FadeInImage.assetNetwork(
+                width: 120,
+                height: 120,
+                placeholder:
+                    "https://avatars0.githubusercontent.com/u/4232933?s=400&v=4",
+                image: state.avatar_url ??
+                    "https://avatars0.githubusercontent.com/u/4232933?s=400&v=4",
               ),
-                Column(
-                  children: <Widget>[
-                    Text(
-                      state.name ?? 'name',
-                      key: Key('user_info_name'),
-                    ),
-                    Text(
-                      state.id.toString() ?? 'id',
-                      key: Key('user_info_id'),
-                    ),
-                    //定位
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.location_on),
-                        Text(
-                          state.location ?? 'location',
-                          key: Key('user_info_location'),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.link),
-                        Text(
-                          state.url ?? 'url',
-                          key: Key('user_info_link'),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.email),
-                        Text(
-                          state.email ?? 'email',
-                          key: Key('email'),
-                        ),
-                      ],
-                    )
-                  ],
+            ),
+            Text(
+              state.name ?? 'name',
+              key: Key('user_info_name'),
+              textAlign: TextAlign.left,
+            ),
+            Text(
+              state.id.toString() ?? 'id',
+              key: Key('user_info_id'),
+            ),
+            //定位
+            Row(
+              children: <Widget>[
+                Icon(Icons.location_on),
+                Text(
+                  state.location ?? 'location',
+                  key: Key('user_info_location'),
                 ),
-                Row(
-                  children: <Widget>[
-                    Text('Flutter'),
-                  ],
-                )
               ],
-            )
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.link),
+                Text(
+                  state.url ?? 'url',
+                  key: Key('user_info_link'),
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.email),
+                Text(
+                  state.email ?? 'email',
+                  key: Key('email'),
+                ),
+              ],
+            ),
+            Text('Flutter'),
           ],
-        ),
-      ),
-    ),
+        )),
   );
 }
